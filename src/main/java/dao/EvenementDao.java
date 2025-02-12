9e76ad7dad09be4903edf9838a24edf81b6b29e9
asalako <augustin-semevo.salako@etudiant.univ-rennes.fr>
2025-02-12 09:23:19 +0100

    Ajout de méthode dans la classe EvenementDao

--- src/main/java/dao/EvenementDao.java
+++ src/main/java/dao/EvenementDao.java
@@ -1,9 +1,33 @@
 package dao;
 
 import entity.Evenement;
+import entity.Organisateur;
+
+import java.util.List;
 
 public class EvenementDao extends AbstractJpaDao<Long, Evenement> {
     public EvenementDao() {
         setClazz(Evenement.class);
+    }
+
+    public List<Evenement> findEvenementsByOrganisateurId(Long organisateur_id) {
+        return entityManager.createQuery(
+                "SELECT e FROM Evenement e WHERE e.organisateur.id = :organisateur_id", Evenement.class)
+                .setParameter("organisateur_id", organisateur_id)
+                .getResultList();
+    }
+
+    public List<Evenement> findEvenementByOrganisateur(Organisateur organisateur) {
+        return entityManager.createQuery(
+                "SELECT e FROM Evenement e WHERE e.organisateur = :organisateur", Evenement.class)
+                .setParameter("organisateur", organisateur)
+                .getResultList();
+    }
+
+    public List<Evenement> findByPrixLessThan(Double prix) {
+        return entityManager.createQuery(
+                "SELECT e FROM Evenement  e WHERE e.prix < :prix", Evenement.class)
+                .setParameter("prix", prix)
+                .getResultList();
     }
 }
