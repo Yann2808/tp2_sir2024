4aadbc0b610d004639a98483694d7bfa48d0fa69
asalako <augustin-semevo.salako@etudiant.univ-rennes.fr>
2025-04-30 00:44:46 +0200

    Modification de Organisateur pour implémenter le système d'authentification basé sur Basic Auth

--- src/main/java/entity/Organisateur.java
+++ src/main/java/entity/Organisateur.java
@@ -1,32 +1,57 @@
 package entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Organisateur extends User{
+@DiscriminatorValue("ORGANISATEUR")
+public class Organisateur extends User {
     private String nomEntreprise;
     private String siret;
 
-    public void setNomEntreprise(String nomEntreprise){
+    @OneToMany(mappedBy = "organisateur", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Evenement> events = new ArrayList<>();
+
+    // Constructeurs
+    public Organisateur() {}
+
+    public Organisateur(String username, String password, String email, String nomEntreprise, String siret) {
+        setUsername(username);
+        setPassword(password);
+        setEmail(email);
+        setRole("ORGANISATEUR");
         this.nomEntreprise = nomEntreprise;
-    }
-
-    public String getNomEntreprise(){
-        return nomEntreprise;
-    }
-
-    public void setSiret(String siret){
         this.siret = siret;
     }
 
-    public String getSiret(){
+    public void setNomEntreprise(String nomEntreprise) {
+        this.nomEntreprise = nomEntreprise;
+    }
+
+    public String getNomEntreprise() {
+        return nomEntreprise;
+    }
+
+    public void setSiret(String siret) {
+        this.siret = siret;
+    }
+
+    public String getSiret() {
         return siret;
     }
 
-    @OneToMany(mappedBy = "organisateur")
-    private List<Evenement> events = new ArrayList<>();
+    public List<Evenement> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Evenement> events) {
+        this.events = events;
+    }
 }
